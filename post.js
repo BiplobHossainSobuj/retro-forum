729f2160e1b0273702f8c95728a6aef56273630b
BiploHossainSobuj <bhs.cse@outlook.com>
2024-03-04 11:53:40 +0600

    finished search functionality

--- post.js
+++ post.js
@@ -1,10 +1,11 @@
-let isMarked = false;
-
-const fetchAllPost = async () => {
+let isSearched = false;
+const fetchAllPost = async (isSearched) => {
   const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
   const data = await res.json();
   const allPosts = data.posts;
-  displayAllpost(allPosts);
+  if (!isSearched) {
+    displayAllpost(allPosts);
+  }
 }
 const fetchLatestPost = async () => {
   const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
@@ -12,8 +13,21 @@ const fetchLatestPost = async () => {
   // console.log(data)
   displayLatestpost(data);
 }
+const fetchPostByCategory = async(category,isSearched)=>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`)
+  const data = await res.json();
+  if (isSearched) {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('hidden');
+    setTimeout(() => {
+      displayAllpost(data.posts);
+      spinner.classList.add('hidden');
+    }, 2000);
+  }
+}
 const displayAllpost = (posts) => {
-  const postsContainer = document.getElementById('discusion-container')
+  const postsContainer = document.getElementById('discusion-container');
+  postsContainer.textContent = '';
   for (const post of posts) {
     if (post.title.includes("'")) {
       post.title = post.title.replace("'", '');
@@ -48,7 +62,7 @@ const displayAllpost = (posts) => {
                   <p class=" lg:ms-2">${post.posted_time} min</p>
                 </div>
               </div>
-              <div id=${post.id} onclick='handleMark("${post.title}","${post.view_count}")' class="card-actions justify-end">
+              <div id=${post.id} onclick='handleMark("${post.title}","${post.view_count}")' class="card-actions justify-end hover:cursor-pointer">
                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                     <path d="M13.9998 0C6.26805 0 9.15527e-05 6.26814 9.15527e-05 13.9999C9.15527e-05 21.7314 6.26805 28 13.9998 28C21.7315 28 27.9999 21.7314 27.9999 13.9999C27.9999 6.26814 21.7315 0 13.9998 0ZM14 4.91741L22.2847 10.0835H5.71542L14 4.91741ZM22.3879 18.333H22.3871C22.3871 19.1616 21.7155 19.8331 20.887 19.8331H7.1131C6.28447 19.8331 5.61303 19.1615 5.61303 18.333V10.4122C5.61303 10.3245 5.62199 10.2393 5.63655 10.1556L13.552 15.0914C13.5617 15.0975 13.5721 15.1016 13.5821 15.1072C13.5925 15.113 13.6032 15.1186 13.6138 15.1239C13.6697 15.1527 13.7273 15.176 13.7862 15.1912C13.7923 15.1929 13.7983 15.1936 13.8044 15.195C13.869 15.2102 13.9344 15.2197 13.9998 15.2197H14.0002C14.0007 15.2197 14.0012 15.2197 14.0012 15.2197C14.0665 15.2197 14.1319 15.2105 14.1965 15.195C14.2026 15.1935 14.2086 15.1929 14.2147 15.1912C14.2735 15.176 14.3309 15.1527 14.3871 15.1239C14.3977 15.1186 14.4084 15.113 14.4188 15.1072C14.4287 15.1016 14.4392 15.0975 14.4489 15.0914L22.3644 10.1556C22.3789 10.2393 22.3879 10.3244 22.3879 10.4122V18.333Z" fill="#10B981"/>
                   </svg>
@@ -56,7 +70,7 @@ const displayAllpost = (posts) => {
           </div>
         `
     if (post.isActive) {
-      postContainer.firstElementChild.classList.add('badge-info');
+      postContainer.firstElementChild.classList.add('badge-success');
     } else {
       postContainer.firstElementChild.classList.add('badge-error');
     }
@@ -64,7 +78,7 @@ const displayAllpost = (posts) => {
   }
 
 }
-
+// handle Total Marked post 
 let totalMarked = 0;
 const handleMark = (title, view) => {
   totalMarked+=1;
@@ -87,12 +101,11 @@ const handleMark = (title, view) => {
   `
   markedPostsContainer.appendChild(div);
 }
-
+// display latest post 
 const displayLatestpost =(posts)=>{
   const latestPostContainer = document.getElementById("latest-post-container");
 
   posts.forEach(post => {
-    console.log(post);
     const div = document.createElement('div');
     div.classList= 'card w-96 h-full bg-base-100 border border-slate-200';
     div.innerHTML = `
@@ -120,5 +133,12 @@ const displayLatestpost =(posts)=>{
   });
 }
 
+const handleSearch = ()=>{
+  isSearched = true;
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  fetchPostByCategory(searchText,isSearched);
+}
+
+fetchAllPost(isSearched);
 fetchLatestPost();
-fetchAllPost();
